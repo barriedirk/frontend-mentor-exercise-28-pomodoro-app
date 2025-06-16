e8f3a17115bfcf83bc1b14a888bc98863952f7f8
Barrie Freyre <barriedirk@yahoo.com>
2025-06-15 22:59:30 -0500

    add setting styles

--- pomodoro-app-project/astro.config.mjs
+++ pomodoro-app-project/astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 const isDev = process.env.NODE_ENV === "development";
+const base = isDev ? "/" : "frontend-mentor-exercise-28-pomodoro-app";
 
 // https://astro.build/config
 export default defineConfig({
-  base: isDev ? "/" : "frontend-mentor-exercise-28-pomodoro-app",
-
+  base,
   compressHTML: false,
   build: {
     assets: "astro_assets",
